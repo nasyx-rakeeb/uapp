4ead6d98252a157879e0d3fdb246b93ccab9b9a5
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-11-03 10:54:01 +0530

    add

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -5,59 +5,28 @@ import Button from "../components/Button.js"
 const Welcome = () => {
   return (
     <View style={styles.root}>
-    <View style={styles.root2}>
-      <View style={styles.imgContainer}>
-        <Image style={styles.img} source={require("../assets/images/welcome.png")} resizeMode="center" />
-      </View>
-      <View style={styles.txtContainer}>
-        <Text style={styles.head1}>Tanesco Tanzania</Text>
-        <Text style={styles.head2}>Tunaangaza maisha yako</Text>
-      </View>
-      <View style={styles.btnContainer}>
-        <Button title="Log In" />
-      </View>
-      </View>
+      <Image style={styles.img} source={require("../assets/images/welcome.png")} resizeMode="center" />
+      <Text style={styles.head1}>Tanesco Tanzania</Text>
+      <Text style={styles.head2}>Tunaangaza maisha yako</Text>
+      <Button title="Log In" />
     </View>
     )
 }
 
 const styles = StyleSheet.create({
   root: {
-    flex: 15,
+    flex: 10,
     backgroundColor: colors.white,
     alignItems: "center"
   },
-  root2: {
-    flex: 1,
-    width: "100%"
-  },
-  imgContainer: {
-    flex: 5,
-    justifyContent: "center",
-    alignItems: "center"
-  },
   img: {
-    flex: 1,
-    aspectRatio: 1,
-    width: "100%",
-    height: undefined
-  },
-  btnContainer: {
-    flex: 5,
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  txtContainer: {
-    flex: 5,
-    alignItems: "center",
-    justifyContent: "flex-end"
+    
   },
   head1: {
     
   },
   head2: {
-    color: colors.silver,
+    
   }
 })
 
